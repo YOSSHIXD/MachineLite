a2e83be727e35433486088038d1c6248ad0fad54
ym <bruh@ez.pz>
2021-05-11 07:19:45 +0900

    バインドされているキーの確認機能の追加

--- src/main/java/com/lite/machinelite/command/impl/BindCommand.java
+++ src/main/java/com/lite/machinelite/command/impl/BindCommand.java
@@ -13,15 +13,20 @@ public class BindCommand extends Command {
     }
 
     public void fire(String[] args) {
-        if (args != null && args.length == 2) {
+        if (args != null && args.length > 0) {
             Module module = MachineLite.getModuleManager().getModuleByString(args[0]);
-            if (module != null) {
-                int key = Keyboard.getKeyIndex(args[1].toUpperCase());
-                module.setKeyCode(key);
-                MachineLite.WriteChat("\2477" + module.getName() + " bind to \247f" + Keyboard.getKeyName(module.getKeyCode()) + "\2477.");
-            } else {
-                MachineLite.WriteChat("\247cModule was not found");
-            }
+
+                if (module != null) {
+                    if (args.length == 1) {
+                        MachineLite.WriteChat(String.format("\2477The current key for \247b%s is \2479%s.", module.getName(), Keyboard.getKeyName(module.getKeyCode())));
+                    } else if (args.length == 2) {
+                        int key = Keyboard.getKeyIndex(args[1].toUpperCase());
+                        module.setKeyCode(key);
+                        MachineLite.WriteChat("\2477" + module.getName() + " bind to \247f" + Keyboard.getKeyName(module.getKeyCode()) + "\2477.");
+                    }
+                } else {
+                    MachineLite.WriteChat("\247cModule was not found");
+                }
         } else {
             MachineLite.WriteChat("\2477.bind <Module> <Key>");
         }
